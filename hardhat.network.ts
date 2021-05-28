7ff6d184f7e2a3f874bc9ce7e4d3c1ce5a0fb8e2
igoriev36 <igoryev26@gmail.com>
2021-05-28 18:12:56 +0530

    change network config

--- hardhat.network.ts
+++ hardhat.network.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 
 const alchemyUrl = process.env.ALCHEMY_URL;
 const infuraApiKey = process.env.INFURA_API_KEY;
-const mnemonic = process.env.HDWALLET_MNEMONIC;
+const mnemonic = process.env.HDWALLET_MNEMONIC;;
 
 const networks: HardhatUserConfig['networks'] = {
   coverage: {
@@ -17,55 +17,23 @@ const networks: HardhatUserConfig['networks'] = {
   },
 };
 
-if (alchemyUrl && process.env.FORK_ENABLED && mnemonic) {
+if (process.env.FORK_ENABLED == "true") {
   networks.hardhat = {
     chainId: 1,
     forking: {
-      url: alchemyUrl,
-      blockNumber: 12226812
+      url: `${alchemyUrl}`,
+      // blockNumber: 12254830
     },
     accounts: {
       mnemonic,
     },
   };
-} else {
+}  else {
   networks.hardhat = {
     allowUnlimitedContractSize: true,
   };
 }
-
-if (mnemonic) {
-  networks.xdai = {
-    chainId: 100,
-    url: 'https://rpc.xdaichain.com/',
-    accounts: {
-      mnemonic,
-    },
-  };
-  networks.poaSokol = {
-    chainId: 77,
-    url: 'https://sokol.poa.network',
-    accounts: {
-      mnemonic,
-    },
-  };
-  networks.matic = {
-    chainId: 137,
-    url: 'https://rpc-mainnet.maticvigil.com',
-    accounts: {
-      mnemonic,
-    },
-  };
-  networks.mumbai = {
-    chainId: 80001,
-    url: 'https://rpc-mumbai.matic.today',
-    accounts: {
-      mnemonic,
-    },
-    loggingEnabled: true,
-  };
-}
-
+  
 if (infuraApiKey && mnemonic) {
   networks.kovan = {
     url: `https://kovan.infura.io/v3/${infuraApiKey}`,
@@ -73,7 +41,7 @@ if (infuraApiKey && mnemonic) {
       mnemonic,
     },
   };
-
+  
   networks.ropsten = {
     url: `https://ropsten.infura.io/v3/${infuraApiKey}`,
     accounts: {
@@ -89,7 +57,7 @@ if (infuraApiKey && mnemonic) {
   };
 
   networks.mainnet = {
-    url: alchemyUrl,
+    url: `${alchemyUrl}`,
     accounts: {
       mnemonic,
     },
